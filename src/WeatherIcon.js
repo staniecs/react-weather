891bdf5d84ae50003e8a64a1aca751ad2599537b
Sarah <145446078+staniecs@users.noreply.github.com>
2024-04-12 13:11:23 +0200

    color coded icons based on day/night

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,32 +3,32 @@ import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function WeatherIcon(props) {
 const codeMapping = {
-    "clear-sky-day": "CLEAR_DAY",
-    "clear-sky-night": "CLEAR_NIGHT",
-    "few-clouds-day": "PARTLY_CLOUDY_DAY",
-    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
-    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
-    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHTY",
-    "broken-clouds-day": "CLOUDY",
-    "broken-clouds-night": "CLOUDY",
-    "shower-rain-day": "RAIN",
-   "shower-rain-night": "RAIN",
-    "rain-day": "RAIN",
-    "rain-night": "RAIN",
-    "thunderstorm-day": "RAIN",
-    "thunderstorm-night": "RAIN",
-    "snow-day": "SNOW",
-    "snow-night": "SNOW",
-    "mist-day": "FOG",
-    "mist-night": "FOG",
+    "clear-sky-day": {icon: "CLEAR_DAY", color: "#b458d8"},
+    "clear-sky-night": {icon: "CLEAR_NIGHT", color: "#69337f"},
+    "few-clouds-day":  {icon: "PARTLY_CLOUDY_DAY", color: "#b458d8"},
+    "few-clouds-night":  {icon: "PARTLY_CLOUDY_NIGHT", color: "#69337f"},
+    "scattered-clouds-day":  {icon: "PARTLY_CLOUDY_DAY", color: "#b458d8"},
+    "scattered-clouds-night":  {icon: "PARTLY_CLOUDY_NIGHT", color: "#69337f"},
+    "broken-clouds-day":  {icon: "CLOUDY", color: "#b458d8"},
+    "broken-clouds-night":  {icon: "CLOUDY", color: "#69337f"},
+    "shower-rain-day":  {icon: "RAIN", color: "#b458d8"},
+    "shower-rain-night":  {icon: "RAIN", color: "#69337f"},
+    "rain-day":  {icon: "RAIN", color: "#b458d8"},
+    "rain-night":  {icon: "RAIN", color: "#69337f"},
+    "thunderstorm-day":  {icon: "RAIN", color: "#b458d8"},
+    "thunderstorm-night":  {icon: "RAIN", color: "#69337f"},
+    "snow-day":  {icon: "SNOW", color: "#b458d8"},
+    "snow-night":  {icon: "SNOW", color: "#69337f"},
+    "mist-day":  {icon: "FOG", color: "#b458d8"},
+    "mist-night":  {icon: "FOG", color: "#69337f"}
 }
 
 
 
 
  return(  <ReactAnimatedWeather
-        icon={codeMapping[props.code]}
-        color= '#8d43a8'
+        icon={codeMapping[props.code].icon}
+        color= {codeMapping[props.code].color}
         size= {52}
         animate={true}
       />)
