1df233c76d0437de135030bbcc9025b79dab7138
Sarah <145446078+staniecs@users.noreply.github.com>
2024-04-12 12:10:54 +0200

    installed react weather icons

---
+++ src/WeatherIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactAnimatedWeather from 'react-animated-weather';
+
+export default function WeatherIcon(props) {
+    return(  <ReactAnimatedWeather
+        icon='CLEAR_DAY'
+        color= 'violet'
+        size= {64}
+        animate={true}
+      />)
+}
